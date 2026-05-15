d8dea3908a2b46753a9ef9588899f4053c22c820
Lari Nurkka <lari.nurkka@gmail.com>
2026-05-15 17:10:51 +0300

    smooth zooming

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -2,10 +2,22 @@ extends Camera2D
 
 @export_range(0.001, 10.0) var min_zoom = 0.1
 @export_range(0.001, 10.0) var max_zoom = 5.0
+@export_range(0.0, 1.0) var zoom_snap_seconds = 0.4
 
 var last_mouse_position = null
+var zoom_tween: Tween = null
+@onready var real_zoom = zoom:
+	set(value):
+		if zoom_tween:
+			zoom_tween.kill()
+		zoom_tween = create_tween() \
+				.bind_node(self) \
+				.set_ease(Tween.EASE_OUT) \
+				.set_trans(Tween.TRANS_CUBIC)
+		zoom_tween.tween_property(self, "zoom", value, zoom_snap_seconds)
+		real_zoom = value
 
-const ZOOM_STEP = Vector2.ONE / 16.0
+const ZOOM_STEP = Vector2.ONE / 8.0
 
 
 func _ready() -> void:
@@ -28,13 +40,14 @@ func _input(event: InputEvent) -> void:
 				else:
 					last_mouse_position = null
 			MOUSE_BUTTON_WHEEL_UP:
-				var next_zoom = zoom - ZOOM_STEP * zoom * event.factor
+				var next_zoom = real_zoom - ZOOM_STEP * zoom * event.factor
 				if next_zoom.x >= min_zoom:
-					zoom = next_zoom
+					real_zoom = next_zoom
 			MOUSE_BUTTON_WHEEL_DOWN:
-				var next_zoom = zoom + ZOOM_STEP * zoom * event.factor
+				var next_zoom = real_zoom + ZOOM_STEP * zoom * event.factor
 				if next_zoom.x <= max_zoom:
-					zoom = next_zoom
+					real_zoom = next_zoom
+
 	elif event is InputEventMouseMotion and last_mouse_position:
 		position += (last_mouse_position - event.position) * (Vector2.ONE / zoom)
 		last_mouse_position = event.position
